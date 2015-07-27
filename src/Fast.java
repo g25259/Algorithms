afd36b22716c51e4aa7a671a1d937793d4e58afa
TMJ <g25259@live.com>
2015-07-27 13:29:15 +0800

    An ugly and not working prototype of Fast.java Still confusing how to solve sub segment problem

--- src/Fast.java
+++ src/Fast.java
@@ -21,24 +21,21 @@ public class Fast {
             copyPoint[i] = points[i];
         }
 
-        for (int i = 0; i < points.length; i++) {
-            Arrays.sort(points, copyPoint[i].SLOPE_ORDER);
-            int collinear = 0;
+        for (int i = 0; i < points.length - 4; i++) {
+            Arrays.sort(points);
+            Arrays.sort(points, i + 1, N - 1, copyPoint[i].SLOPE_ORDER);
+            int collinear = 1;
             int startIndex = -1;
-            int endIndex = -1;
-            double preSlope = Double.NEGATIVE_INFINITY;
-            for (int j = 0; j < points.length; j++) {
-                double cuSlope = points[j].slopeTo(copyPoint[i]);
-                if (cuSlope == Double.NEGATIVE_INFINITY) continue;
-                if (preSlope == cuSlope) {
+            Double preSlope = copyPoint[i].slopeTo(points[i+1]);
+            for (int j = i + 2; j < points.length; j++) {
+                Double cuSlope = points[j].slopeTo(copyPoint[i]);
+                if (preSlope.equals(cuSlope)) {
                     collinear++;
-                    if (collinear == 1) {
-                        startIndex = j;
+                    if (collinear == 2) {
+                        startIndex = j - 1;
                     }
                 } else {
                     if (collinear >= 4) {
-                        endIndex = j;
-
                         Point[] collinearP = new Point[collinear];
                         for (int k = 0; k < collinear; k++) {
                             collinearP[k] = points[startIndex + k];
@@ -51,10 +48,23 @@ public class Fast {
                                 StdOut.print(collinearP[m] + " -> ");
                         }
                     }
-                    collinear = 0;
+                    collinear = 1;
                 }
                 preSlope = cuSlope;
 
+            }
+            if (collinear >= 4) {
+                Point[] collinearP = new Point[collinear];
+                for (int k = 0; k < collinear; k++) {
+                    collinearP[k] = points[startIndex + k];
+                }
+                Arrays.sort(collinearP);
+                for (int m = 0; m < collinearP.length; m++) {
+                    if (m == collinearP.length - 1)
+                        StdOut.print(collinearP[m]);
+                    else
+                        StdOut.print(collinearP[m] + " -> ");
+                }
             }
         }
     }
