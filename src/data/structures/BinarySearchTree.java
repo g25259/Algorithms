de44a09bbe5984dfe09e9640d4b60ef547a7620c
g2525_000 <g25259@live.com>
2015-08-04 14:38:26 +0800

    Processing BST, only left delete function

--- src/data/structures/BinarySearchTree.java
+++ src/data/structures/BinarySearchTree.java
@@ -27,14 +27,23 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     }
 
     private Node put(Node x, Key k, Value v) {
-        if (x == null) return new Node(k, v, 0);
+        if (x == null) return new Node(k, v, 1);
         int cmp = k.compareTo(x.key);
         if (cmp > 0) x.right = put(x.right, k, v);
         else if (cmp < 0) x.left = put(x.left, k, v);
         else x.value = v;
-        x.count = x.left.count + x.right.count + 1;
+        x.count = size(x.left) + size(x.right) + 1;
         return x;
 
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) return 0;
+        return x.count;
     }
 
     public Value get(Key k) {
@@ -49,7 +58,35 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     }
 
     public void delete(Key k) {
+        delete(k, root);
+    }
 
+    public Node delete(Key k, Node x) {
+        if (x == null) return null;
+        int cmp = k.compareTo(x.key);
+        if (cmp < 0) x.left = delete(k, x.left);
+        else if (cmp > 0) x.right = delete(k, x.right);
+        else {
+            if (x.left == null) return x.right;
+            if (x.right == null) return x.left;
+
+            Node t = x;
+
+
+        }
+        x.count = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    public Node min(Node x) {
+        if (x.left == null) return x;
+        return min(x.left);
+    }
+    public Node deleteMin(Node x) {
+        if (x.left == null) return x.right;
+        x.left = deleteMin(x.left);
+        x.count = 1 + size(x.left) + size(x.right);
+        return x;
     }
 
     public Key floor(Key k) {
@@ -62,12 +99,41 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         int cmp = k.compareTo(x.key);
         if (cmp == 0) return x;
         if (cmp < 0) return floor(k, x.left);
-        if (cmp > 0 ) {
-            Node canidate = x;
-            x = floor(k, x.right);
-            if (x == null) return canidate;
-            return x;
-        }
+
+        Node canidate = x;
+        x = floor(k, x.right);
+        if (x == null) return canidate;
+        return x;
+
+    }
+
+    public Key ceil(Key k) {
+        Node ceil = ceil(k, root);
+        return ceil.key;
+    }
+
+    private Node ceil(Key k, Node x) {
+        if (x == null) return null;
+        int cmp = k.compareTo(x.key);
+        if (cmp > 0) return ceil(k, x.right);
+        else if (cmp == 0) return x;
+
+        Node canidate = x;
+        x = ceil(k, x.left);
+        if (x == null) return canidate;
+        return x;
+    }
+
+    public int rank(Key k) {
+        return rank(k, root);
+    }
+
+    private int rank(Key k, Node x) {
+        if (x == null) return 0;
+        int cmp = k.compareTo(x.key);
+        if (cmp == 0) return size(x.left);
+        if (cmp < 0) return rank(k, x.left);
+        else return size(x.left) + 1 + rank(k, x.right);
     }
 
     public Queue<Key> inOrder() {
@@ -100,12 +166,22 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 
 
     public static void main(String[] args) {
-        BinarySearchTree<Double, Integer> bst = new BinarySearchTree<>();
-        for (int i = 0; i < 10; i++) {
-            bst.put((Math.random() * 10 + 1), (int) (Math.random() * 50 + 1));
-        }
+        BinarySearchTree<String, Integer> bst = new BinarySearchTree<>();
+        bst.put("S", 1);
+        bst.put("X", 1);
+        bst.put("E", 1);
+        bst.put("A", 1);
+        bst.put("C", 1);
+        bst.put("R", 1);
+        bst.put("H", 1);
+        bst.put("M", 1);
 
-        for (Double item : bst.iterator()) System.out.println(item);
+        System.out.println(bst.floor("G"));
+        System.out.println(bst.ceil("L"));
+        System.out.println(bst.rank("G"));
+        bst.delete("E");
+        for (String item : bst.iterator()) System.out.println(item);
+
     }
 
 
