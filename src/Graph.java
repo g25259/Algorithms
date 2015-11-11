b5afddc8d493b39497ba1c8d11491a06b6d16ac4
MingJe <g25259@live.com>
2015-11-12 01:19:28 +0800

    Start Algorithms Part II Start Graph API

---
+++ src/Graph.java
@@ -0,0 +1,62 @@
+/**
+ * Created by MingJe on 2015/11/12.
+ */
+public class Graph {
+    private final int V;
+    private Bag<Integer>[] adj;
+    private int degree;
+
+    public Graph(int V) {
+        this.V = V;
+        adj = new Bag[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new Bag();
+        }
+    }
+
+    public Graph(In in) {
+        V = Integer.parseInt(in.readLine());
+        adj = new Bag[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new Bag();
+        }
+        int E = Integer.parseInt(in.readLine());
+        for (int i = 0; i < E; i++) {
+            String line = in.readLine();
+            String[] vertex = line.split(" ");
+            addEdge(Integer.parseInt(vertex[0]),
+                    Integer.parseInt(vertex[1]));
+        }
+
+
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        degree += 2;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return degree /= 2;
+    }
+
+    public String toString() {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph(new In());
+        System.out.println(graph);
+    }
+
+}
