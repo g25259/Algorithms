68f26136fb3a7cc5ecf70c46f0ea71ef905d37b6
MingJe <g25259@live.com>
2015-12-20 16:57:42 +0800

    HW4 97% version Ratio = 3.xx

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -1,6 +1,4 @@
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
-import edu.princeton.cs.algs4.SET;
 import edu.princeton.cs.algs4.StdOut;
 
 import java.util.HashSet;
@@ -38,7 +36,7 @@ public class BoggleSolver {
         public int get(String key) {
             Node x = get(root, key, 0);
             if (x == null) return -1;
-            return  x.value;
+            return x.value;
         }
 
         public Node get(Node x, String key, int d) {
@@ -58,6 +56,7 @@ public class BoggleSolver {
     private TrieST dictionary;
     private boolean[][] used;
     private StringBuilder prefix;
+    private HashSet<String> validWords;
 
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
@@ -73,87 +72,105 @@ public class BoggleSolver {
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        HashSet<String> validWords = new HashSet<>();
+        validWords = new HashSet<>();
         used = new boolean[board.rows()][board.cols()];
         for (int i = 0; i < board.rows(); i++) {
             for (int j = 0; j < board.cols(); j++) {
-                dfs(board, i, j, validWords, dictionary.root);
+                dfs(board, i, j, dictionary.root);
             }
         }
         return validWords;
     }
 
-    private void dfs(BoggleBoard board, int row, int col, HashSet<String> validWords, TrieST.Node x) {
+    private void dfs(BoggleBoard board, int row, int col, TrieST.Node x) {
         if (row > 0) {
-            boolean isEnter = check(board, row - 1, col, validWords, x);
-            if (isEnter) used[row - 1][col] = false;
+            if (!used[row - 1][col]) {
+                check(board, row - 1, col, x);
+                // used[row - 1][col] = false;
+            }
             if (col > 0) {
-                isEnter = check(board, row - 1, col - 1, validWords, x);
-                if (isEnter) used[row - 1][col - 1] = false;
+                if (!used[row - 1][col - 1]) {
+                    check(board, row - 1, col - 1, x);
+                    // used[row - 1][col - 1] = false;
+                }
             }
             if (col < board.cols() - 1) {
-                isEnter = check(board, row - 1, col + 1, validWords, x);
-                if (isEnter) used[row - 1][col + 1] = false;
+                if (!used[row - 1][col + 1]) {
+                    check(board, row - 1, col + 1, x);
+                    //  used[row - 1][col + 1] = false;
+                }
             }
         }
         if (row < board.rows() - 1) {
-            boolean isEnter = check(board, row + 1, col, validWords, x);
-            if (isEnter) used[row + 1][col] = false;
+            if (!used[row + 1][col]) {
+                check(board, row + 1, col, x);
+                // used[row + 1][col] = false;
+            }
             if (col > 0) {
-                isEnter = check(board, row + 1, col - 1, validWords, x);
-                if (isEnter) used[row + 1][col - 1] = false;
+                if (!used[row + 1][col - 1]) {
+                    check(board, row + 1, col - 1, x);
+                    // used[row + 1][col - 1] = false;
+                }
             }
             if (col < board.cols() - 1) {
-                isEnter = check(board, row + 1, col + 1, validWords, x);
-                if (isEnter) used[row + 1][col + 1] = false;
+                if (!used[row + 1][col + 1]) {
+                    check(board, row + 1, col + 1, x);
+                    // used[row + 1][col + 1] = false;
+                }
             }
         }
         if (col > 0) {
-            boolean isEnter = check(board, row, col - 1, validWords, x);
-            if (isEnter) used[row][col - 1] = false;
+            if (!used[row][col - 1]) {
+                check(board, row, col - 1, x);
+                // used[row][col - 1] = false;
+            }
         }
         if (col < board.cols() - 1) {
-            boolean isEnter = check(board, row, col + 1, validWords, x);
-            if (isEnter) used[row][col + 1] = false;
+            if (!used[row][col + 1]) {
+                check(board, row, col + 1, x);
+                // used[row][col + 1] = false;
+            }
         }
     }
 
-    private boolean check(BoggleBoard board, int row, int col, HashSet<String> validWords, TrieST.Node x) {
+    private void check(BoggleBoard board, int row, int col, TrieST.Node x) {
         char letter = board.getLetter(row, col);
-
-        if (!used[row][col]) {
-
-            if (letter == 'Q') {
-                prefix.append(letter);
-                prefix.append('U');
-                x = x.next['Q' - 'A'];
-                if (x != null)
-                    x = x.next['U' - 'A'];
+        if (letter == 'Q') {
+            x = x.next['Q' - 'A'];
+            if (x != null) {
+                x = x.next['U' - 'A'];
             } else {
-                prefix.append(letter);
-                x = x.next[letter - 'A'];
+                return;
             }
-            if (x != null && x.prefix == 1) {
-                used[row][col] = true;
+        } else {
+            x = x.next[letter - 'A'];
+        }
+        if (x != null && x.prefix == 1) {
+            used[row][col] = true;
+            if (letter != 'Q'){
+                prefix.append(letter);
                 if (prefix.length() >= 3) {
                     if (x.value != -1)
                         validWords.add(prefix.toString());
                 }
-                dfs(board, row, col, validWords, x);
+                dfs(board, row, col, x);
+                used[row][col] = false;
+                prefix.deleteCharAt(prefix.length() - 1);
             }
 
-            if (letter == 'Q') {
-                prefix.deleteCharAt(prefix.length() - 1);
-                prefix.deleteCharAt(prefix.length() - 1);
-            } else prefix.deleteCharAt(prefix.length() - 1);
-
-
-            return true;
+            else {
+                prefix.append(letter);
+                prefix.append('U');
+                if (prefix.length() >= 3) {
+                    if (x.value != -1)
+                        validWords.add(prefix.toString());
+                }
+                dfs(board, row, col, x);
+                used[row][col] = false;
+                prefix.delete(prefix.length() - 2, prefix.length());
+            }
         }
-        return false;
-
     }
-
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
